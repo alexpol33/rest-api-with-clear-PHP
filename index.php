1d1c61e8f23717199df6dc1acd7a28804fcb0b04
Alexpol <alexpol2905@gmail.com>
2021-08-08 17:33:00 +0300

    last settings

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL);
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Credentials: true');
 header('Content-type: json/application');
 include 'vendor/connect.php';
 include 'vendor/functions.php';
